b2bd40eb9591441d9085d9945076c7cea5db86e1
Habeeb Kotun Jr <hjrkotun@gmail.com>
2015-06-25 09:48:05 -0700

    The bear animation, flying penguin, and seal .ccb files have been created. The physics for the penguin and the seal have been initialized. The Penguin and Seal classes have not been written in Xcode yet.

---
+++ PeevedPenguins.spritebuilder/Source/Seal.swift
@@ -0,0 +1,13 @@
+//
+//  Seal.swift
+//  PeevedPenguins
+//
+//  Created by Habeeb Kotun Jr. on 6/25/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Seal: CCSprite {
+   
+}
